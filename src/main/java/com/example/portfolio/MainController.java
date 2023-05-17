12de74151e9ae49111bbe636d0c9fa55d8c97785
gusnl88 <gusnl88@naver.com>
2023-05-17 10:21:34 +0900

    수정

---
+++ src/main/java/com/example/portfolio/MainController.java
@@ -0,0 +1,12 @@
+package com.example.portfolio;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String fistPage(){
+       return "main";
+    }
+}
