59d91149ccd21504197d901f7ccd0737abb11620
gusnl88 <gusnl88@naver.com>
2023-05-17 10:25:45 +0900

    수정중!

--- src/main/java/com/example/portfolio/MainController.java
+++ src/main/java/com/example/portfolio/MainController.java
@@ -2,10 +2,12 @@ package com.example.portfolio;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    @GetMapping
+
+    @GetMapping("/main.do")
     public String fistPage(){
        return "main";
     }
