b152c0fb69f0b7442ef5dc4f4f773f9d8ae282b4
gusnl88 <gusnl88@naver.com>
2023-05-18 14:23:42 +0900

    프로필

--- src/main/java/com/example/portfolio/MainController.java
+++ src/main/java/com/example/portfolio/MainController.java
@@ -15,4 +15,8 @@ public class MainController {
     public String index(){
         return "index";
     }
+    @GetMapping("profile.do")
+    public String profile(){
+        return "profile";
+    }
 }
