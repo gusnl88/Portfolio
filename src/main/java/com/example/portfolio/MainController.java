89a137c2ae573ebb8c5d90f3540f9071ba2a2c40
gusnl88 <gusnl88@naver.com>
2023-09-18 14:22:28 +0900

    업데이트

--- src/main/java/com/example/portfolio/MainController.java
+++ src/main/java/com/example/portfolio/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
 
     @GetMapping("/main.do")
     public String fistPage(){
-       return "main";
+       return "/main/main";
     }
     @GetMapping("/index.do")
     public String index(){
